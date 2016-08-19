ef69da3d665eefa7f513f8336eb42839a643607d
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-08-19 18:13:43 +0300

    test passes

--- test/steemex_test.exs
+++ test/steemex_test.exs
@@ -5,8 +5,8 @@ defmodule SteemexTest do
 
   test "client call fn for sending a msg to WS process jsonrpc and receives an async msg response" do
     test_pid = self()
-    handler_fn = fn data ->
-      send(test_pid, data)
+    handler_fn = fn id, call_params, data  ->
+      send(test_pid, {id, call_params, data})
     end
 
     Steemex.IdAgent.start_link
@@ -16,7 +16,7 @@ defmodule SteemexTest do
 
     Steemex.call(example_params)
 
-    assert_receive {["database_api", "get_dynamic_global_properties", []], %{"id" => _, "result" => _}}, 5_000
+    assert_receive {_, ["database_api", "get_dynamic_global_properties", []], %{"id" => _, "result" => _}}, 5_000
   end
 
 
