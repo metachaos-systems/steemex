80568951d1846b98393c72eebdcc11e2a791bd86
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-08-26 18:46:36 +0300

    more tests

--- test/steemex_test.exs
+++ test/steemex_test.exs
@@ -1,21 +1,45 @@
 defmodule SteemexTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Steemex
 
-
-  test "get_dynamic_global_properties call succeeds" do
-    test_pid = self()
-    handler_fn = fn id, call_params, data  ->
-      send(test_pid, {id, call_params, data})
-    end
+  setup_all context do
     Steemex.IdAgent.start_link
-    Steemex.WS.start_link(handler_fn, System.get_env("STEEM_URL"))
-    example_params = ["database_api", "get_dynamic_global_properties", []]
-
-    Steemex.call(example_params)
-
-    assert_receive {_, ["database_api", "get_dynamic_global_properties", []], %{"id" => _, "result" => _}}, 5_000
+    handler_fn = fn pid ->
+      fn id, call_params, data  ->
+        send(pid, {id, call_params, data})
+      end
+    end
+    %{handler_fn: handler_fn, steem_url: System.get_env("STEEM_URL")}
   end
+
+  test "get_dynamic_global_properties call succeeds", context do
+    handler_fn = context.handler_fn.(self)
+    Steemex.WS.start_link(handler_fn, context.steem_url)
+
+    params = ["database_api", "get_dynamic_global_properties", []]
+    Steemex.call(params)
+
+    assert_receive {_, params, %{"id" => _, "result" => _}}, 5_000
+  end
+
+  test "get_content", context do
+    handler_fn = context.handler_fn.(self)
+    Steemex.WS.start_link(handler_fn, context.steem_url)
+    Steemex.get_content("xeroc", "piston-web-first-open-source-steem-gui---searching-for-alpha-testers")
+    params = ["database_api", "get_content", ["xeroc", "piston-web-first-open-source-steem-gui---searching-for-alpha-testers"]]
+
+    assert_receive {_, params, %{"id" => _, "result" => %{"author" => _, "permlink" => _}}}, 5_000
+  end
+
+  test "get_block", context do
+    handler_fn = context.handler_fn.(self)
+    Steemex.WS.start_link(handler_fn, context.steem_url)
+    Steemex.get_block(3_141_592)
+    params = ["database_api", "get_block", [3_141_592]]
+
+    assert_receive {_, params, %{"id" => _, "result" => %{"previous" => _, "transactions" => _}}}, 5_000
+  end
+
 
 
 
