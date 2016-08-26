2513626a1a35ac0b28ae5c540848126680ba7ece
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-08-26 19:33:29 +0300

    params need hats!

--- test/steemex_test.exs
+++ test/steemex_test.exs
@@ -20,7 +20,7 @@ defmodule SteemexTest do
     params = [@db_api, "get_dynamic_global_properties", []]
     Steemex.call(params)
 
-    assert_receive {_, params, %{"id" => _, "result" => _}}, 5_000
+    assert_receive {_, ^params, %{"id" => _, "result" => _}}, 5_000
   end
 
   test "get_content", context do
@@ -29,7 +29,7 @@ defmodule SteemexTest do
     Steemex.get_content("xeroc", "piston-web-first-open-source-steem-gui---searching-for-alpha-testers")
     params = [@db_api, "get_content", ["xeroc", "piston-web-first-open-source-steem-gui---searching-for-alpha-testers"]]
 
-    assert_receive {_, params, %{"id" => _, "result" => %{"author" => _, "permlink" => _}}}, 5_000
+    assert_receive {_, ^params, %{"id" => _, "result" => %{"author" => _, "permlink" => _}}}, 5_000
   end
 
   test "get_block", context do
@@ -38,7 +38,7 @@ defmodule SteemexTest do
     Steemex.get_block(3_141_592)
     params = [@db_api, "get_block", [3_141_592]]
 
-    assert_receive {_, params, %{"id" => _, "result" => %{"previous" => _, "transactions" => _}}}, 5_000
+    assert_receive {_, ^params, %{"id" => _, "result" => %{"previous" => _, "transactions" => _}}}, 5_000
   end
 
 end
