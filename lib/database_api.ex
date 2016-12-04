8c5415c5549b52eb98f2f5345ea413592532f100
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-12-04 21:29:38 +0200

    move db methods to their module

--- lib/database_api.ex
+++ lib/database_api.ex
@@ -1,8 +1,44 @@
 defmodule Steemex.DatabaseApi do
 
-    def call(method, params) do
-      Steemex.call(["database_api"], method, [params])
-    end
+  def call(method, params) do
+    Steemex.call(["database_api"], method, [params])
+  end
 
 
+  def get_block(height) do
+    call("get_block", [height]]
+  end
+
+  def get_content(author, permlink) do
+    call("get_content", [author, permlink]]
+  end
+
+  def get_accounts(accounts) do
+    accounts = List.wrap(accounts)
+    call("get_accounts", [accounts])
+  end
+
+  def get_block_header(height) do
+    call("get_block_header", [height]])
+  end
+
+  def get_dynamic_global_properties do
+    call("get_dynamic_global_properties", []])
+  end
+
+  def get_chain_properties do
+    call("get_chain_properties", []])
+  end
+
+  def get_feed_history do
+    call("get_feed_history", []])
+  end
+
+  def get_current_median_history_price do
+    call("get_current_median_history_price", []])
+  end
+
+  defp call_db(method, params) do
+    call( method, params])
+  end
 end
--- lib/steemex.ex
+++ lib/steemex.ex
@@ -37,42 +37,6 @@ defmodule Steemex do
   end
 
 
-  def get_block(height) do
-    call [@db_api, "get_block", [height]]
-  end
-
-  def get_content(author, permlink) do
-    call [@db_api, "get_content", [author, permlink]]
-  end
-
-  def get_accounts(accounts) do
-    accounts = List.wrap(accounts)
-    call([@db_api, "get_accounts", [accounts]])
-  end
-
-  def get_block_header(height) do
-    call([@db_api, "get_block_header", [height]])
-  end
-
-  def get_dynamic_global_properties do
-    call([@db_api, "get_dynamic_global_properties", []])
-  end
-
-  def get_chain_properties do
-    call([@db_api, "get_chain_properties", []])
-  end
-
-  def get_feed_history do
-    call([@db_api, "get_feed_history", []])
-  end
-
-  def get_current_median_history_price do
-    call([@db_api, "get_current_median_history_price", []])
-  end
-
-  defp call_db(method, params) do
-    call([@db_api, method, params])
-  end
 
   @doc """
   Sends an event to the WebSocket server
