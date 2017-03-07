ab74c8bcfe912c015a178a075fee105659cfac10
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-03-07 12:53:20 +0200

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@
 
   def project do
     [app: :steemex,
-     version: "0.8.0",
+     version: "0.9.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
