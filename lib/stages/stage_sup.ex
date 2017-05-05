3c1d99cc13363a82f962c7cfb53fb42636f9774e
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-05-05 18:25:02 +0300

    add producers supervisor

---
+++ lib/stages/stage_sup.ex
@@ -0,0 +1,21 @@
+defmodule Steemex.ProducerSupervisor do
+  use Supervisor
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, :ok, name: :golos_producer_sup)
+  end
+
+  def init(:ok) do
+    children = [
+      worker(
+        Steemex.Stage.Blocks.Producer, [[], [name: Steemex.Stage.Blocks.Producer] ]
+      ),
+      worker(
+        Steemex.Stage.Ops.ProducerConsumer, [%{subscribe_to: [Steemex.Stage.Blocks.Producer]}, [name: Steemex.Stage.Ops.ProducerConsumer] ]
+      ),
+    ]
+    supervise(children, strategy: :one_for_all)
+  end
+
+
+end
