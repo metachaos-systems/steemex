5fd503dc864f7854e6dcd83af9b970c97d044d2e
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-06-07 21:35:23 +0300

    update example to match latest datatypes

--- lib/stages/example_stage_consumer.ex
+++ lib/stages/example_stage_consumer.ex
@@ -1,5 +1,6 @@
 defmodule Steemex.Stage.Ops.ExampleConsumer do
   use GenStage
+  alias Steemex.StructuredOps
   require Logger
 
   def start_link(args, options \\ []) do
@@ -12,12 +13,23 @@ defmodule Steemex.Stage.Ops.ExampleConsumer do
 
   def handle_events(events, _from, state) do
     for op <- events do
-      Logger.info """
-      New operation:
-      #{inspect op}
-      """
+      process_event(op)
     end
     {:noreply, [], state}
+  end
+
+  def process_event({%StructuredOps.Reblog{} = op_data, %{height: h, timestamp: t} =  op_metadata}) do
+      Logger.info """
+      New reblog:
+      #{inspect op_data} at height #{h} and time #{t}
+      """
+  end
+
+  def process_event({op_data, %{height: h, timestamp: t} =  op_metadata}) do
+      Logger.info """
+      New operation:
+      #{inspect op_data} at height #{h} and time #{t}
+      """
   end
 
 end
