f0908bda4c114f6c7f0e3c7c8873588466d73d6f
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-06-07 14:14:09 +0300

    comment transform:red

--- test/op_transformers_test.exs
+++ test/op_transformers_test.exs
@@ -9,5 +9,11 @@ defmodule Steemex.Ops.TransformersTest do
     assert prepared == %{to: "bob", from: "alice", amount: 100.0, token: "SBD", memo: "nice cypher you've got there"}
   end
 
+  test "comment op cleaned correctly " do
+    op = %{body: "body1", title: "", author: "alice", permlink: "permlink1", json_metadata: "{\"tags\":[\"tag1\"],\"app\":\"steemit/0.1\"}", parent_author: "parent_author1", parent_permlink: "parent_permlink1"}
+    prepared = Steemex.Ops.Transform.prepare_for_db(op)
+    json_metadata = %{tags: ["tag1"], app: "steemit/0.1"}
+    assert prepared == %{body: "body1", title: nil, author: "alice", permlink: "permlink1", tags: json_metadata.tags, app: json_metadata.app, json_metadata: json_metadata, parent_author: "parent_author1", parent_permlink: "parent_permlink1"}
+  end
 
 end
