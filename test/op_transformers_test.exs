d78b0f687ff730e917d17a435cfc1f9bec29861c
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-06-07 18:07:34 +0300

    follow op transform:red

--- test/op_transformers_test.exs
+++ test/op_transformers_test.exs
@@ -5,27 +5,27 @@ defmodule Steemex.Ops.TransformersTest do
 
   test "transfer op cleaned correctly " do
     op = %Ops.Transfer{to: "bob", from: "alice", amount: "100 SBD", memo: "nice cypher you've got there"}
-    prepared = Steemex.Ops.Transform.prepare_for_db(op)
+    prepared = Ops.Transform.prepare_for_db(op)
     assert prepared == %{to: "bob", from: "alice", amount: 100.0, token: "SBD", memo: "nice cypher you've got there"}
   end
 
   test "comment op cleaned correctly " do
     op = %Ops.Comment{body: "body1", title: "", author: "alice", permlink: "permlink1", json_metadata: "{\"tags\":[\"tag1\"],\"app\":\"steemit/0.1\"}", parent_author: "parent_author1", parent_permlink: "parent_permlink1"}
-    prepared = Steemex.Ops.Transform.prepare_for_db(op)
+    prepared = Ops.Transform.prepare_for_db(op)
     json_metadata = %{tags: ["tag1"], app: "steemit/0.1"}
     assert prepared == %{body: "body1", title: nil, author: "alice", permlink: "permlink1", tags: json_metadata.tags, app: json_metadata.app, json_metadata: json_metadata, parent_author: "parent_author1", parent_permlink: "parent_permlink1"}
   end
 
   test "comment with no parent_author op cleaned correctly " do
     op = %Ops.Comment{body: "body1", title: "", author: "alice", permlink: "permlink1", json_metadata: "{\"tags\":[\"tag1\"],\"app\":\"steemit/0.1\"}", parent_author: "", parent_permlink: "category"}
-    prepared = Steemex.Ops.Transform.prepare_for_db(op)
+    prepared = Ops.Transform.prepare_for_db(op)
     json_metadata = %{tags: ["tag1"], app: "steemit/0.1"}
     assert prepared == %{body: "body1", title: nil, author: "alice", permlink: "permlink1", tags: json_metadata.tags, app: json_metadata.app, json_metadata: json_metadata, parent_author: nil, parent_permlink: "category"}
   end
 
   test "follow op cleaned and parsed correctly " do
     op = %Ops.CustomJson{id: "follow", json: "[\"follow\",{\"follower\":\"follower1\",\"following\":\"following1\",\"what\":[\"blog\"]}]", "required_auths": [], "required_posting_auths": ["follower1"]}
-    prepared = Steemex.Ops.Transform.prepare_for_db(op)
+    prepared = Ops.Transform.prepare_for_db(op)
     assert prepared == %{follower: "follower1", following: "following1", what: ["blog"]}
   end
 
