f60d0c296927323b85e2eeee16f0b60349145f47
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-06-07 13:22:06 +0300

    improve code readability, add commented consumer example worker()

--- lib/stages/stage_sup.ex
+++ lib/stages/stage_sup.ex
@@ -1,18 +1,16 @@
 defmodule Steemex.Stage.Supervisor do
   use Supervisor
-
+  alias Steemex.Stage
   def start_link do
     Supervisor.start_link(__MODULE__, :ok, name: Steemex.Stage.Supervisor)
   end
 
   def init(:ok) do
+    blocks_producer = Stage.Blocks.Producer
     children = [
-      worker(
-        Steemex.Stage.Blocks.Producer, [[], [name: Steemex.Stage.Blocks.Producer] ]
-      ),
-      worker(
-        Steemex.Stage.Ops.ProducerConsumer, [%{subscribe_to: [Steemex.Stage.Blocks.Producer]}, [name: Steemex.Stage.Ops.ProducerConsumer] ]
-      ),
+      worker( Stage.Blocks.Producer, [[], [name: blocks_producer] ] ),
+      worker( Stage.Ops.ProducerConsumer, [%{subscribe_to: [blocks_producer]}, [name: Stage.Ops.ProducerConsumer] ] ),
+      # worker( Stage.Ops.ExampleConsumer, [%{subscribe_to: [Stage.Ops.ProducerConsumer]}])
     ]
     supervise(children, strategy: :one_for_all)
   end
