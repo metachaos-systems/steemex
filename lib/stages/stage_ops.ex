df5c733434b4b04a4d86e2cad93ff1ef18aea200
ontofractal <ontofractal@protonmail.com>
2017-06-23 15:54:48 +0100

    update stage ops

--- lib/stages/stage_ops.ex
+++ lib/stages/stage_ops.ex
@@ -16,7 +16,7 @@ defmodule Steemex.Stage.Ops do
     {:noreply, List.flatten(events), number}
   end
 
-  def unpack_and_convert_operations(block) do
+  def unpack_and_convert_operations(%Steemex.Event{data: block}) do
      for tx <- block.transactions do
       for op <- tx.operations do
         convert_to_event(op, block)
