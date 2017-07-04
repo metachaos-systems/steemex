90443f30aa01398f7a00a115f24d259354e1f2d6
ontofractal <ontofractal@protonmail.com>
2017-07-04 11:59:28 +0100

    move raw data parsing to block mod

--- lib/block.ex
+++ lib/block.ex
@@ -2,4 +2,8 @@ defmodule Steemex.Block do
   defstruct [:height, :previous, :extensions, :timestamp, :transaction_merkle_root, :transactions, :witness, :witness_signature]
 
   use ExConstructor
+
+  def parse_raw_data(block) do
+    Map.put(block, :timestamp, block.timestamp <> "Z" |> NaiveDateTime.from_iso8601!())
+  end
 end
