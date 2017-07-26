97f2731cfcae38b51a16efe115e18a7103cb6274
ontofractal <ontofractal@protonmail.com>
2017-07-26 13:13:32 +0100

    add response cleaner

---
+++ lib/cleaner.ex
@@ -0,0 +1,12 @@
+defmodule Steemex.Cleaner do
+
+  def strip_token_names_and_convert_to_number(data) do
+    to_clean = ~w(max_accepted_payout promoted total_payout_value pending_payout_value total_pending_payout_value
+    curator_payout_value promoted balance vesting_withdraw_rate
+    savings_balance savings_sbd_balance sbd_balance vesting_balance vesting_shares)a
+    for {k,v} <- data, into: %{} do
+       {k, (if k in to_clean, do: v |> Float.parse |> elem(0), else: v)}
+    end
+  end
+
+end
