9b43c9f22bc1f7ea3eb375eea1b69c777b57a7a8
ontofractal <ontofractal@protonmail.com>
2017-10-26 18:51:59 +0100

    erlang websocket_client removed, elixir websockex added

---
+++ lib/steemex/ws_next.ex
@@ -0,0 +1,34 @@
+defmodule Steemex.WSNext do
+  use WebSockex
+  require Logger
+
+  def start_link(url, opts \\ []) do
+    uri = URI.parse(url)
+    opts = Keyword.merge(opts, [name: :steemex_ws])
+    WebSockex.start_link(url, __MODULE__, %{}, opts)
+  end
+
+
+  def handle_connect(_conn, state) do
+    Logger.info("Golos websocket is connected!")
+    {:ok, state}
+  end
+
+  def handle_frame({:text, msg}, state) do
+    data = Poison.decode!(msg)
+    id = data["id"]
+    {from, params} = Steemex.IdStore.get(id)
+    send(from, {:ws_response, {id, params, data}})
+    {:ok, state}
+  end
+
+  def handle_disconnect(%{reason: {:local, reason}}, state) do
+    Logger.info("Local close with reason: #{inspect reason}")
+    {:ok, state}
+  end
+
+  def handle_disconnect(disconnect_map, state) do
+    super(disconnect_map, state)
+  end
+
+end
