08bc6f7fff67295b39aee0fd9c26773d3905f68d
ontofractal <ontofractal@protonmail.com>
2018-05-06 17:24:39 +0100

    update tests

---
+++ config/test.exs
@@ -0,0 +1,6 @@
+config :steemex,
+  # api: :steemit_api,
+  # api_url: "https://api.steemit.com",
+  api: :jsonrpc_wss_api,
+  api_url: "wss://gtg.steem.house:8090",
+  activate_stage_sup: false
--- test/steemex_test.exs
+++ test/steemex_test.exs
@@ -4,73 +4,64 @@ defmodule SteemexTest do
   @db_api "database_api"
 
   setup_all context do
-    {:ok, pid} = Steemex.WSNext.start_link("wss://ws.golos.io/")
-    {:ok, pid} = Steemex.IdStore.start_link
+    if Application.get_env(:steemex, :api) == :jsonrpc_wss_api do
+      {:ok, pid} = Steemex.WS.start_link(Application.get_env(:steemex, :api_url))
+      {:ok, pid} = Steemex.IdStore.start_link()
+    end
 
-
-    # %{
-    #   params:
-    #   %{
-    #     glob_dyn_prop: [@db_api, "get_dynamic_global_properties", []],
-    #     get_block: [@db_api, "get_block", [3_141_592]]
-    #   }
-    # }
     :ok
   end
 
-  test "get_dynamic_global_properties call succeeds", context do
-    params = context.params.glob_dyn_prop
-    {:ok, result} = Steemex.call(params)
-
-    assert %{:"head_block_id" => _} = result
-  end
-
   test "get_content" do
-    {:ok, data} = Steemex.get_content("xeroc", "piston-web-first-open-source-steem-gui---searching-for-alpha-testers")
+    {:ok, data} =
+      Steemex.get_content(
+        "xeroc",
+        "piston-web-first-open-source-steem-gui---searching-for-alpha-testers"
+      )
 
-    assert %{:"author" => _, :"permlink" => _} = data
+    assert %{:author => _, :permlink => _} = data
     assert data.created.year === 2016
   end
 
   test "get_block" do
     {:ok, data} = Steemex.get_block(3_141_592)
 
-    assert %Steemex.Block{:"previous" => _, :"transactions" => _} = data
+    assert %Steemex.Block{:previous => _, :transactions => _} = data
   end
 
   test "get_accounts with multiple args" do
     {:ok, data} = Steemex.get_accounts(["dan", "ned"])
-    assert %{:"name" => _, :"id" => _} = hd(data)
+    assert %{:name => _, :id => _} = hd(data)
   end
 
   test "get_block_header" do
     {:ok, data} = Steemex.get_block_header(1)
-    assert %{:"timestamp" => "2016-10-18T11:01:48"} = data
+    assert %{:timestamp => "2016-03-24T16:05:00"} = data
   end
 
   test "get_dynamic_global_properties" do
     {:ok, data} = Steemex.get_dynamic_global_properties()
-    assert %{:"average_block_size" => _,:"confidential_sbd_supply" => _} = data
+    assert %{average_block_size: _, confidential_sbd_supply: _} = data
   end
 
   test "get_chain_properties" do
     {:ok, data} = Steemex.get_chain_properties()
-    assert %{:"account_creation_fee" => _, :"maximum_block_size" => _} = data
+    assert %{:account_creation_fee => _, :maximum_block_size => _} = data
   end
 
   test "get_feed_history" do
     {:ok, data} = Steemex.get_feed_history()
-    assert %{:"current_median_history" => %{:"base" => _}} = data
+    assert %{:current_median_history => %{:base => _}} = data
   end
 
   test "get_config" do
     {:ok, data} = Steemex.get_config()
-    assert %{:"VESTS_SYMBOL" => _} = data
+    assert %{:VESTS_SYMBOL => _} = data
   end
 
   test "get_current_median_history_price" do
     {:ok, data} = Steemex.get_current_median_history_price()
-    assert  %{:"base" => _, :"quote" => _} = data
+    assert %{:base => _, :quote => _} = data
   end
 
   test "get_account_count" do
@@ -79,34 +70,33 @@ defmodule SteemexTest do
   end
 
   test "lookup_accounts" do
-    {:ok, data} =  Steemex.lookup_accounts("steempunks", 10)
+    {:ok, data} = Steemex.lookup_accounts("steempunks", 10)
     assert is_list(data)
-    assert is_bitstring(hd data)
+    assert is_bitstring(hd(data))
   end
 
   test "lookup_account_names" do
     {:ok, data} = Steemex.lookup_account_names(["ontofractal"])
-    assert %{:"name" =>  "ontofractal"} = hd(data)
+    assert %{:name => "ontofractal"} = hd(data)
   end
 
   test "get_account_history" do
     {:ok, data} = Steemex.get_account_history("ontofractal", -1, 10)
-    assert [_, %{:"block" => _}] = hd(data)
+    assert [_, %{:block => _}] = hd(data)
   end
 
   test "get_witness_schedule" do
     {:ok, data} = Steemex.get_witness_schedule()
-    assert %{:"current_shuffled_witnesses" => _} = data
+    assert %{:current_shuffled_witnesses => _} = data
   end
 
   test "get_hardfork_version" do
     {:ok, data} = Steemex.get_hardfork_version()
-    assert "0" <> minor_ver  = data
+    assert "0" <> minor_ver = data
   end
 
   test "get_next_scheduled_hardfork" do
     {:ok, data} = Steemex.get_next_scheduled_hardfork()
-    assert %{:"hf_version" => _, :"live_time" => _}  = data
+    assert %{:hf_version => _, :live_time => _} = data
   end
-
 end
