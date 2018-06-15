3a8fb7033740c4c78761b51d672b407d371174d4
ontofractal <ontofractal@protonmail.com>
2018-06-15 12:07:22 +0100

    mitigate jsonrpc lib not returning a success tuple correctly

--- lib/steemex/steemit_api_client.ex
+++ lib/steemex/steemit_api_client.ex
@@ -4,12 +4,17 @@ defmodule Steemex.SteemitApiClient do
   @url Application.get_env(:steemex, :api_url) || "https://api.steemit.com"
 
   def call([api, method, args]) do
-    api = case api do
-      "database_api" -> "condenser_api"
-      _ -> throw("This JSONRPC api is not supported")
-    end
-    response = HTTP.call(@url, "#{api}.#{method}", args)
-    {:ok, response}
-  end
+    api =
+      case api do
+        "database_api" -> "condenser_api"
+        _ -> throw("This JSONRPC api is not supported")
+      end
 
+    response = HTTP.call(@url, "#{api}.#{method}", args)
+
+    case response do
+      {:error, reason} -> {:error, reason}
+      r -> {:ok, r}
+    end
+  end
 end
